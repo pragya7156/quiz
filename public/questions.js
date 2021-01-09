57dd28be19be1929454f9a3eb6f3752a9b33756a
Pragya Singh <pragyapriya0703@gmail.com>
2021-01-09 18:14:58 +0530

    basic requirements done

--- public/questions.js
+++ public/questions.js
@@ -1,18 +1,27 @@
 var count = 0;
-let user_input = [];
+var c = 0;
 function answer() {
+    var ans = [1, 2, 1, 3, 4, 2, 3, 4, 1, 1];
     let ele = document.getElementsByTagName('input');
-    for(let i = 0;i<ele.length;i++)
-    if(ele[i].checked === true)
-    user_input.push(ele[i].value);
-    let corr = document.querySelectorAll('.correct');
-    if(user_input.length !== corr.length)
-    alert('Please select any one option from each question!!');
+    let q = document.getElementsByClassName('head');
+    for (let i = 0; i < ele.length; i++)
+        if (ele[i].checked === true)
+            c++;
+            console.log(c);
+    if (c !== q.length) {
+        alert("Please answer all the questions!!");
+    }
     else {
-        for(let i=0;i<corr.length;i++)
-        if(user_input[i] === corr[i].value)
-        count++;
+        if (ele[0].checked == true)
+            console.log('hello');
+        for (let i = 0; i < q.length; i++) {
+            for (let j = i * 4; j < (i * 4) + 4; j++) {
+                if (j - (i * 4) + 1 === ans[i] && ele[j].checked === true) {
+                    count++;
+                }
+            }
+        }
         alert(`Your total score : ${count}`);
-    }   
+    }
 }
 
