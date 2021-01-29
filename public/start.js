7e27f1cef189eb3ba772285e1e3b83f2ce69c406
Pragya Singh <pragyapriya0703@gmail.com>
2021-01-29 21:11:23 +0530

    feature modified

--- public/start.js
+++ public/start.js
@@ -1,17 +1,40 @@
 
 var e = document.getElementById('st_btn');
 e.addEventListener('click', ()=>{
-  auth.onAuthStateChanged(user => {
-    if(user) {
-      //console.log(user);
+  auth.signOut()
+  .then((user) => {
+    // Signed out
+    // ...
+    //console.log(user);
+    //alert('you are signed out');
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
       var d = document.getElementById('questions');
       var a = document.getElementById('authseen');
       var f = document.getElementById('first');
+      d.style.cssText = 'display: none';
+      a.style.cssText = 'display: block';
+      f.style.cssText = 'display: none';
+  auth.onAuthStateChanged(user => {
+    if(user) {
+      if(user.emailVerified) {
+      //console.log(user);
       var mail = document.getElementById('dismail');
+      var d = document.getElementById('questions');
+      var a = document.getElementById('authseen');
+      var f = document.getElementById('first');
       mail.innerHTML = `Logged in as: <b> ${user.email} </b>` ;
       d.style.cssText = 'display: block';
       a.style.cssText = 'display: none';
       f.style.cssText = 'display: none';
+      }
+      else {
+        alert('Please verify your email first');
+      }
     }
     else {
       alert('you need to login first');
@@ -33,11 +56,8 @@ login.addEventListener('click', function(){
     var pass = document.getElementById('password').value.trim();
     const auth = firebase.auth();
     auth.signInWithEmailAndPassword(email, pass)
-  .then((user) => {
-    // Signed in 
-    // ...
+  .then((user) => {                           
     //console.log(user);
-    alert('signed in');
   })
   .catch((error) => {
     var errorCode = error.code;
@@ -45,15 +65,22 @@ login.addEventListener('click', function(){
     alert(errorMessage);
   });
 })
-signup.addEventListener('click', function(e){
-  e.preventDefault();
+signup.addEventListener('click', function(){
     var email = document.getElementById('mail').value.trim();
     var pass = document.getElementById('password').value.trim();
     firebase.auth().createUserWithEmailAndPassword(email, pass)
-  .then((user) => {
+  .then((userCredential) => {
     // Signed in 
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  alert('Verification link sent to your email address.')
+}).catch(function(error) {
+  // An error happened.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  alert(errorMessage);
+});
     // ...
-    alert('new user created');
   })
   .catch((error) => {
     var errorCode = error.code;
@@ -61,4 +88,5 @@ signup.addEventListener('click', function(e){
     alert(errorMessage);
     // ..
   });
+    
 })
